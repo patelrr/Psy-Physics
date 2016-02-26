19d3aa0f65b62c19b1cac5db824d35f14d5dffa9
Ravi Patel <ravipatel21111995@gmail.com>
2016-02-26 14:17:40 +0530

    Delete create skin

--- core/src/com/mygdx/game/States/MenuState.java
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -31,7 +31,7 @@ public class MenuState extends State {
         playBtn = new Texture(Gdx.files.internal("data/logo.png"));
         font = new BitmapFont();
         font.setColor(Color.RED);
-        createBasicSkin();
+        //createBasicSkin();
         dropSound.setLooping(true);
         dropSound.play();
         dropSound.setVolume(1.0f);
@@ -63,28 +63,5 @@ public class MenuState extends State {
 
 
         sb.end();
-    }
-    Skin skin;
-    private void createBasicSkin(){
-        //Create a font
-        BitmapFont font = new BitmapFont();
-        skin = new Skin();
-        skin.add("default", font);
-
-        //Create a texture
-        Pixmap pixmap = new Pixmap((int)Gdx.graphics.getWidth()/4,(int)Gdx.graphics.getHeight()/10, Pixmap.Format.RGB888);
-        pixmap.setColor(Color.WHITE);
-        pixmap.fill();
-        skin.add("background",new Texture(pixmap));
-
-        //Create a button style
-        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
-        textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
-        textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
-        textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
-        textButtonStyle.font = skin.getFont("default");
-        skin.add("default", textButtonStyle);
-
     }
 }
